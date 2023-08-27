66b695078ede263353f130fe57e3de0170104433
hafsa-aarifeen <hafsama-im19042@stu.kln.ac.lk>
2023-08-27 21:12:06 +0530

    Edit Todo Components

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -15,10 +15,24 @@ function TodoList() {
         setTodos(newTodos);
     };
 
+    const completeTodo = id => {
+      let updatedTodos = todos.map(todo => {
+        if (todo.id === id) {
+          todo.isComplete = !todo.isComplete;
+        }
+        return todo;
+      });
+      setTodos(updatedTodos);
+    };
+
   return (
     <div>
         <h1>What's the plan for Today ?</h1>
         <TodoForm onSubmit={addTodo} />
+        <Todo
+        todos={todos}
+        completeTodo={completeTodo}
+      />
     </div>
   )
 }
